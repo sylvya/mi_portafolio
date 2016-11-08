766d562a3a1cd95d3c62a866d911f9a54ee0cae5
sylvya <silvia_9010@hotmail.com>
2016-11-08 10:45:52 -0600

    cambios iniciales de proyecto

--- wordpress/wp-content/themes/PortafolioPersonal/header.php
+++ wordpress/wp-content/themes/PortafolioPersonal/header.php
@@ -3,13 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Portafolio Profesional</title>
+	<link rel="stylesheet" href="<?php bloginfo(stylesheet_url); ?>">
 </head>
 <body>
 	
 	<header><!--menú-->
 		<ul>
-			<li><a href="#">Inicio</a></li>
-			<li><a href="#">A cerca de Mí</a></li>
+			<li><a href="#">A cerca de mí</a></li>
+			<li><a href="#">Habilidades</a></li>
 			<li><a href="#">Portafolio</a></li>
 			<li><a href="#">Contacto</a></li>
 		</ul>
--- wordpress/wp-content/themes/PortafolioPersonal/index.php
+++ wordpress/wp-content/themes/PortafolioPersonal/index.php
@@ -1,7 +1,18 @@
 <?php get_header(); ?>
-	<section><!--contenido-->
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus tempora sapiente minima? Nam eveniet accusamus quidem nihil consectetur, architecto quos natus magnam! Modi nihil similique veniam, doloremque delectus alias dignissimos.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet ipsum, error dicta tenetur non, tempore amet voluptatibus dolores dolorem nulla accusantium harum, eum ex rem omnis. Labore dolorum dolores nihil?
-		</p>
+	<section class="content-principal"><!--contenido-->
+		<article class="">
+			<figure>
+				<img src="" alt="">		
+			</figure>
+			<div class="descSitio">
+				<p>
+					Hola. Soy Silvia, ahora estas navegando en mi portafolio profesional, mediante el cual expongo una muestra de mi trabajo, asi como las herramientas que domino, impulsando el mejor aprovechamiento y eficiencia de la web. 
+					Disfrutalo!...
+				</p>
+			</div>
+		</article>
+		<article class="">
+			
+		</article>		
 	</section>
 <?php get_footer(); ?>
